0299bb5d7f773b370d3565c2f2306329f07c60c2
Paulo Fagundes <paulo.fagundes@gfi.pt>
2018-09-18 02:24:34 +0100

    !fix: Instalar todas as dependencias NPM de dev para poder gerar os ficheiros de dist

--- deploy.sh
+++ deploy.sh
@@ -114,7 +114,7 @@ eval $DEPLOYMENT_TARGET
 # 3. Install NPM packages
 if [ -e "$DEPLOYMENT_TARGET/web/app/themes/sns24/package.json" ]; then
   cd "$DEPLOYMENT_TARGET/web/app/themes/sns24"
-  eval $NPM_CMD install --production
+  eval $NPM_CMD install
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
