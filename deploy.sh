7f58a58786b3ab2a569b5509f189a61fec4fd936
Paulo Fagundes <paulo.fagundes@gfi.pt>
2018-09-18 12:15:34 +0100

    no message

--- deploy.sh
+++ deploy.sh
@@ -123,7 +123,7 @@ if [ -e "$DEPLOYMENT_TARGET/web/app/themes/sns24/composer.json" ]; then
   pwd
 #  eval "$DEPLOYMENT_TARGET/web/app/themes/sns24"
 #  echo "php composer.phar --working-dir=$DEPLOYMENT_TARGET/web/app/themes/sns24 install"
-  composer.phar install
+  php composer.phar install
   exitWithMessageOnError "composer failed"
   cd - > /dev/null
 fi
