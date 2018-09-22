665aada9ef98ef588df7bfaeb5f55ea2e466fc7b
Paulo Fagundes <paulo.fagundes@gfi.pt>
2018-09-22 02:13:44 +0100

    Identificadas as noticias

--- web/app/themes/sns24/resources/views/noticias-page.blade..php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<h2>Noticias blade</h2>
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.page-header')
-    @include('partials.content-page')
-  @endwhile
-@endsection
