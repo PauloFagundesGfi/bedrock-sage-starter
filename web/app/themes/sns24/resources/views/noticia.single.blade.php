47e149f2be0c9e982c47857894877402c93dd88a
Paulo Fagundes <paulo.fagundes@gfi.pt>
2018-09-22 01:25:36 +0100

    detalhe???

---
+++ web/app/themes/sns24/resources/views/noticia.single.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h3>Seré este o detalhe de uma notícia????</h3>
+  @while(have_posts()) @php the_post() @endphp
+    @include('partials.content-single-'.get_post_type())
+  @endwhile
+@endsection
